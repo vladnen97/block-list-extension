e4b56a80af0c893c9b7ae40cee83d095c2b655e8
vladnen97 <darkdescent1991@gmail.com>
2024-07-19 19:30:12 +0300

    fix(server): import module instead service

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { DbModule } from '../db/db.module'
-import { AccountService } from '../account/account.service'
+import { AccountModule } from '../account/account.module'
 
 @Module({
-  imports: [DbModule, AccountService],
+  imports: [DbModule, AccountModule],
   providers: [UsersService],
   exports: [UsersService],
 })
