606b8909e8f2ca106dc6e6a6243c9142510b9ae7
vladnen97 <darkdescent1991@gmail.com>
2024-07-22 03:48:16 +0300

    feat(client): add session entity

--- client/src/features/auth/model/use-sign-out.ts
+++ client/src/features/auth/model/use-sign-out.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import { authControllerSignOut } from '@/shared/api/generated'
 import { ROUTES } from '@/shared/constants/routes'
 import { useResetSession } from '@/entities/session/queries'
