3d0391a146623fc1afd8f3fa9c2a3c182717cf9f
vladnen97 <darkdescent1991@gmail.com>
2024-07-29 08:37:33 +0300

    build(extension): install tailwind

--- extension/src/pages/home.tsx
+++ extension/src/pages/home.tsx
@@ -3,5 +3,9 @@ import { useSession } from '@entities/session'
 export const HomePage = () => {
   const { data } = useSession()
 
-  return <div>Home page {data?.email}</div>
+  return (
+    <div className={'text-3xl font-bold text-teal-600'}>
+      Home page {data?.email}
+    </div>
+  )
 }
